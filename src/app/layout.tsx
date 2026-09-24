3f7924836a3dd8fbd6048842b2a2d508e571e65d
Admin <Scheinen.co@gmail.com>
2026-09-24 12:06:49 +0100

    Rip out next-themes entirely and use pure CSS media queries for OS-enforced dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,6 @@ export const metadata: Metadata = {
   },
 };
 
-import { ThemeProvider } from "@/components/providers/theme-provider";
 import { FaviconSwitcher } from "@/components/favicon-switcher";
 
 export default function RootLayout({
@@ -36,20 +35,13 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body suppressHydrationWarning className="min-h-full flex flex-col bg-background text-foreground">
         <NextAuthProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <FaviconSwitcher />
-            <Header />
-            <main className="flex-1 flex flex-col">
-              {children}
-            </main>
-            <Footer />
-            <Toaster richColors position="top-center" />
-          </ThemeProvider>
+          <FaviconSwitcher />
+          <Header />
+          <main className="flex-1 flex flex-col">
+            {children}
+          </main>
+          <Footer />
+          <Toaster richColors position="top-center" />
         </NextAuthProvider>
       </body>
     </html>
